934af05c46be861e8d6e5824c212bbb8224136de
Rodrigo Mamede <61851910+Rodrigosdk@users.noreply.github.com>
2022-03-25 10:04:01 -0300

    created game mode choice

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from tkinter import Menu
 import pygame
 import pygame_menu
 
@@ -9,10 +8,18 @@ def main():
     menu = pygame_menu.Menu('Forca', 600, 400,
                            theme=pygame_menu.themes.THEME_BLUE)
 
-    menu.add.button('Iniciar')
+    menu.add.button('Iniciar', mode_choice)
     menu.add.button('Historico')
     menu.add.button('Sair', pygame_menu.events.EXIT)
 
+    menu.mainloop(surface)
+
+def mode_choice():
+    menu = pygame_menu.Menu('Escolha Uma opção', 600, 400,
+                       theme=pygame_menu.themes.THEME_BLUE)
+    menu.add.button(' P1 vs IA')
+    menu.add.button('P1 vs P2')
+    menu.add.button('Voltar', main)
     menu.mainloop(surface)
 
 
